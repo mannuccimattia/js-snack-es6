5ef415ba5a9887023c836521e74108acc873275e
mannuccimattia <mannuccimattia@gmail.com>
2025-03-28 14:19:00 +0100

    [UPD] Snack 1: done array

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// SNACK 1: 
+// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare in console la bici con peso minore.
+
+// creo l'array 
+const cycles = [
+  {
+    brand: "Atala",
+    weight: 3000
+  },
+  {
+    brand: "Pinarello",
+    weight: 2800
+  },
+  {
+    brand: "Legnano",
+    weight: 2200
+  },
+  {
+    brand: "Graziella",
+    weight: 3800
+  },
+];
+
