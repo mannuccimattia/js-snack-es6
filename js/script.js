bbb73098e63204b685505b9562c52baad548f8ff
mannuccimattia <mannuccimattia@gmail.com>
2025-03-28 14:25:17 +0100

    [UPD] Snack 2: done array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // SNACK 1: 
-// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare in console la bici con peso minore.
+// Creare un array di oggetti.
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. 
+// Stampare in console la bici con peso minore.
 
 // creo l'array 
 const cycles = [
@@ -35,3 +37,42 @@ for(i=0; i<cycles.length; i++){
 
 // stampo in console
 console.log(lighter.brand + " è la bici più leggera (" + lessWeight + " gr).");
+
+
+
+// SNACK 2:
+// Creare un array di oggetti di squadre di calcio. 
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. 
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. 
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”. 
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+// creo l'array
+const teams = [
+  {
+    teamName: "Team 1",
+    points: 0,
+    faultNum: 0
+  },
+  {
+    teamName: "Team 2",
+    points: 0,
+    faultNum: 0
+  },
+  {
+    teamName: "Team 3",
+    points: 0,
+    faultNum: 0
+  },
+  {
+    teamName: "Team 4",
+    points: 0,
+    faultNum: 0
+  },
+  {
+    teamName: "Team 5",
+    points: 0,
+    faultNum: 0
+  },
+]
+console.log(teams)
